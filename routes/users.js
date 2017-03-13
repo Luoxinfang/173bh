b7ea27f52234357462a9e193fba37e0aa26bdfab
roy <xf.radish@gmail.com>
2017-03-13 23:44:13 +0800

    update

--- routes/users.js
+++ routes/users.js
@@ -5,5 +5,8 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
-
+router.post('/', function(req, res, next) {
+  console.log(req.body);
+  res.send('respond with a json');
+});
 module.exports = router;
